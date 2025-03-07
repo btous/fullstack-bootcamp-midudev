f39cbd0e940d338897a6547b1a4be2ed561daaff
Bernat Tous <bernat.tous@gmail.com>
2025-03-07 08:42:31 +0100

    Add basic Express server with CRUD routes for notes
    This commit introduces an Express server with endpoints for managing notes, including GET, POST, and DELETE operations. It includes initial mock data, middleware for logging, and error handling. The project setup also includes package.json with dependencies and configurations for development utilities like StandardJS.

---
+++ part3-fullstack/index.js
@@ -0,0 +1,72 @@
+import notes from './notes.js'
+import express from 'express'
+import logger from './middlewares/logger.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(logger)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hola món</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find((note) => note.id.toString() === id)
+  note
+    ? response.json(note)
+    : response.status(404).json({
+      error: 'Note not found'
+    })
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  if (!note) {
+    return response.status(400).json({
+      error: 'Note is missing'
+    })
+  }
+  if (!note.content) {
+    return response.status(400).json({
+      error: 'Note content is required'
+    })
+  }
+  const notesIds = notes.map((note) => note.id)
+  const uniqueId = notesIds.length ? Math.max(...notesIds) + 1 : 1
+  const newNote = {
+    id: uniqueId,
+    content: note.content,
+    date: new Date().toISOString(),
+    important: typeof note.important === 'boolean' ? note.important : false
+  }
+  notes.push(newNote)
+  response.status(201).json(newNote)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const noteIndex = notes.findIndex((note) => note.id.toString() === id)
+  if (noteIndex === -1) {
+    return response.status(404).end()
+  }
+  notes.splice(noteIndex, 1)
+  response.status(204).end()
+})
+
+app.use((request, response) => {
+  response.status(404).json({
+    error: 'Not found'
+  })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ part3-fullstack/notes.js
@@ -0,0 +1,39 @@
+const notes = [
+  {
+    id: 1,
+    content: 'This is a new note',
+    date: '2021-03-12T12:00:00.000Z',
+    important: false,
+    user: 1
+  },
+  {
+    id: 2,
+    content: 'This is another note',
+    date: '2021-03-12T12:00:00.000Z',
+    important: true,
+    user: 1
+  },
+  {
+    id: 3,
+    content: 'This is a third note',
+    date: '2021-03-12T12:00:00.000Z',
+    important: false,
+    user: 2
+  },
+  {
+    id: 4,
+    content: 'This is a fourth note',
+    date: '2021-03-12T12:00:00.000Z',
+    important: true,
+    user: 2
+  },
+  {
+    id: 5,
+    content: 'This is a fifth note',
+    date: '2021-03-12T12:00:00.000Z',
+    important: false,
+    user: 3
+  }
+]
+
+export default notes
