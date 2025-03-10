0cb7c14d020720ecb75dd79acc6bd0d59e324847
Bernat Tous <bernat.tous@gmail.com>
2025-03-10 11:50:34 +0100

    Add CORS support to the backend
    Added the `cors` package to handle Cross-Origin Resource Sharing and enabled it in the Express application. This update improves compatibility with frontend applications by allowing requests from different origins.

--- part3-fullstack/index.js
+++ part3-fullstack/index.js
@@ -1,9 +1,11 @@
 import notes from './notes.js'
 import express from 'express'
+import cors from 'cors'
 import logger from './middlewares/logger.js'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use(logger)
